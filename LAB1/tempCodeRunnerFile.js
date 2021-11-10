fd4115faca7482b04c2afdda2bd647a2fc8757d7
t610112418046 <60768317+t610112418046@users.noreply.github.com>
2021-11-10 16:01:41 +0700

    Add files via upload

---
+++ LAB1/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+console.log(add(5, 3));
+console.log(addFn(4, 5));
+
+//es6
+const addEs6 = (a, b) => a * b;
+let multiAdd = (a, b) => {
+    let c = a * b;
+    return c;
+}
+
+console.log(addEs6(10, 5));
+console.log(multiAdd(4, 2));
